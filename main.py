e469f378794846dca93217dedfbd1604f51c07d8
1qzuser <111686044+1qzuser@users.noreply.github.com>
2023-09-10 19:13:38 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,43 +1,37 @@
-import requests, json, re, os
+import requests
 
-session = requests.session()
-# 机场的地址
-url = os.environ.get('URL')
-# 配置用户名（一般是邮箱）
-email = os.environ.get('EMAIL')
-# 配置用户名对应的密码 和上面的email对应上
-passwd = os.environ.get('PASSWD')
-# server酱
-SCKEY = os.environ.get('SCKEY')
+def checkin_with_push(Email, passwd, SCKEY):
+    url = "https://go.runba.cyou/auth/login"
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36 Edg/109.0.1518.69'
+    }
+    data = {
+        "email": Email,
+        "passwd": passwd
+    }
+    resp = requests.post(url, headers=headers, data=data)
+    to_set_cookie = requests.utils.dict_from_cookiejar(resp.cookies)
+    if not (resp.status_code == 200 and resp.json().get('ret') == 1):
+        print("登录失败", resp.text)
+        exit(0)
+    else:
+        print('用户==>', to_set_cookie.get('email'), '登录成功')
+    checkin_url = "https://go.runba.cyou/user/checkin"
+    resp2 = requests.post(checkin_url, headers=headers, cookies=to_set_cookie)
+    if resp2.status_code == 200:
+        if resp2.json().get("ret") == 1:
+            print("*" * 10 + "签到成功" + 10 * "*")
+            print("签到获得流量==>", resp2.json().get('msg'))
+            print("剩余流量==>", resp2.json().get('trafficInfo').get('unUsedTraffic'))
+            print("已经使用==>", resp2.json().get('trafficInfo').get('lastUsedTraffic'))
+            print("今日使用==>", resp2.json().get('trafficInfo').get('todayUsedTraffic'))
+            # 进行推送
+            if SCKEY != '':
+                push_url = 'https://sctapi.ftqq.com/{}.send?title=机场签到&desp={}'.format(SCKEY, resp2.json().get('msg'))
+                requests.post(url=push_url)
+                print('推送成功')
+        else:
+            print(resp2.json().get("msg"))
+    return resp2
 
-login_url = '{}/auth/login'.format(url)
-check_url = '{}/user/checkin'.format(url)
-
-
-header = {
-        'origin': url,
-        'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
-}
-data = {
-        'email': email,
-        'passwd': passwd
-}
-try:
-    print('进行登录...')
-    response = json.loads(session.post(url=login_url,headers=header,data=data).text)
-    print(response['msg'])
-    # 进行签到
-    result = json.loads(session.post(url=check_url,headers=header).text)
-    print(result['msg'])
-    content = result['msg']
-    # 进行推送
-    if SCKEY != '':
-        push_url = 'https://sctapi.ftqq.com/{}.send?title=机场签到&desp={}'.format(SCKEY, content)
-        requests.post(url=push_url)
-        print('推送成功')
-except:
-    content = '签到失败'
-    print(content)
-    if SCKEY != '':
-        push_url = 'https://sctapi.ftqq.com/{}.send?title=机场签到&desp={}'.format(SCKEY, content)
-        requests.post(url=push_url)
+checkin_with_push('1366565528@qq.com', 'cyx2174324', 'SCT179362T7SSeEEZcUVRSyTxPTt6YiYtS')
